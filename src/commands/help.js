d4354e74db4287b0743b71e9dac4dee91d943d09
NekoSakuraLucia <mokurolucia@gmail.com>
2025-01-06 18:18:38 +0700

    Add a help command to view the list of all commands available in the bot.

---
+++ src/commands/help.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { helpEmbed, NoCommandFound } from '../utils/embeds/help/index.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Displays a list of available commands'),
+  async execute(interaction) {
+    const commands = interaction.client.commands;
+
+    if (!commands || commands.size === 0) {
+      return interaction.reply({
+        embeds: [NoCommandFound()],
+        ephemeral: true
+      });
+    }
+
+    await interaction.reply({ embeds: [helpEmbed(commands, interaction)] });
+  }
+};
